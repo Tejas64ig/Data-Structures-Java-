75a1d69b58a41010dffed518eb638dcdac076620
Who cares <Thakurtejas567@gmail.com>
2026-01-19 23:44:41 +0530

    Add files via upload

---
+++ Linked Lists/SingleLinkList/LL for 3 nodes_GetNode Fn/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+
+  static class Node{
+    int data;
+    Node Next;
+
+    Node(int val){
+        data=val;
+        Next=null;
+
+    }
+      static Node head = null;
+      static Node getNode(int val) {
+          return new Node(val);
+      }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static void main (){}
