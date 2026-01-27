749ce332daac74a348452874e53f5247821e6bb1
Who cares <Thakurtejas567@gmail.com>
2026-01-27 18:15:49 +0530

    Update Main.java

--- Linked Lists/SingleLinkList/Reverse_SLL/Main.java
+++ Linked Lists/SingleLinkList/Reverse_SLL/Main.java
@@ -1,24 +1,69 @@
-public class Main{
+public class Main {
+
     public static class Node {
+
         int Data;
         Node Next;
-        Node(int val){
-            Data=val;
+
+        Node(int val) {
+            Data = val;
             Next = null;
         }
-      //  Node Head = null;
 
-        public static Node Reverse(Node Head)
-        {
-            Node Temp= null;
+        static Node Head = null;
 
-          Temp  =Head.Next;
+        static Node getNode(int val) {
+            return new Node(val);
+        }
 
+        // Revere
+        static Node Revere(Node start) {
+            Node rev = null;
+            Head = start;
+            while (Head != null) {
 
+                Head = start.Next;
+                start.Next = rev;
+                rev = start;
+                start = Head;
+            }
 
-
-            return Head;
+            return rev;
         }
 
     }
+
+    //             Display
+    static void Display(Node Head) {
+        if (Head == null) {
+            return;
+        }
+
+        Node temp = Head;
+        while (temp != null) {
+            System.out.print(temp.Data + " ");
+            temp = temp.Next;
+        }
+    }
+
+
+    public static void main() {
+        Node head = null;
+        Node Rev_Head = null;
+
+        head = Node.getNode(10);
+        head.Next = Node.getNode(20);
+        head.Next.Next = Node.getNode(30);
+
+        Display(head);
+
+        // reverse
+System.out.println("\nReverse SLL");
+        head = Node.Revere(head);
+
+        Display(head);
+    }
 }
+
+
+
